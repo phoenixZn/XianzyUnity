814acf83f4e399bf65f0e99a8062a6617b29b8eb
9364932 <9364932@qq.com>
2026-05-29 17:49:25 +0800

    整理忽略文件 + HybridCLR相关Settings.asset文件

--- Assets/HotScripts/Framework/GameEntry.Unity.cs
+++ Assets/HotScripts/Framework/GameEntry.Unity.cs
@@ -1,35 +1,5 @@
 using UnityEngine;
-/*
-热更程序集结构说明：
-├── HotUpdate/
-│   ├── Framework/               跨项目框架约束共识 ----------------------------------------------------------------
-│   │   ├── BaseUtility/         #公共程序集 （这里的代码所有项目，各处都依赖，都会引用，避免修改）
-│   │   │	├── DataStruct/     （公共数据结构）
-│   │   │	└── Helper/         （公共静态方法）
-│   │   ├── CoreGame/            #核心玩法，局内战斗
-│   │   ├── Modules/             #数据模块
-│   │   ├── Services/            #有框架接口规范约束的、业务无关的底层服务（过去每个项目都有的 AssetMng、EventMng、TaskMng、NetMng、ConfigMng等等 ） 
-│   │   │   ├── Interface/      （定义公司全项目复用的基础设施 接口层约束，IAssetService、IPBConfigService、INetService等）
-│   │   ├── GEnv                 #全局环境，唯一官方单例，拼装所有的 Service、Module、Managers（过去的GameGlobal）
-│   │   └── GameEntry       #AOT调用热更程序集的入口 （负责创建GEnv，驱动GEnv，和AOT程序集中的启动器交互）
-│   │
-│   │
-│   ├── LiteGame/                小游戏扩展框架 ----------------------------------------------------------------
-│   │   ├── BaseUtility/
-│   │   ├── CoreGame/
-│   │   │   ├── CustomNodes.Ex/      （CustomLogic的Nodes扩展）
-│   │   │   ├── ECWorldEx/           （ECWorld扩展）
-│   │   │   ├── ECWorldPack.Base/    （ECWorld基本组件包）
-│   │   │   └── ECWorldPack.Entt/    （ECWorld扩展组件包.Entt关联）
-│   │   ├── Modules/                  #数据模块
-│   │   ├── Managers/                （GEnv中 不受框架限制的自由Mng)
-│   │   ├── Services/                （GEnv中 受框架接口约束的Mng)
-│   │   │   ├── Service.GameObjectPool/
-│   │   │   ├── Service.UI/
-│   │   │   └── Service.Entt/
-│   │   ├── GEnv.LiteGame
-│   │   └── GameEntry.LiteGame
- */
+
 
 
 namespace HotUpdate
